7d2ac3967398cc5d7dc866570284e401efaf7494
bhatta09 <sandesh@academiacollege.edu.np>
2024-04-22 13:48:41 +0545

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import img from "../assests/img/contactimg.jpg"
+// import img from "../assests/img/contactimg.jpg"
 import Button from '../layouts/Button'
 
 const Contact = () => {
     return (
+        
         <div className='min-h-screen flex flex-col items-center justify-center md:mx-32 mx-5 mt-10'>
+            <h1>Contact</h1>
             <div className='flex flex-col md:flex-row justify-between w-full'>
                 <form className='w-full md:w-2/5 space-y-5 pt-20'>
                     <h1 className='text-5xl font-semibold text-center'>Contact <span className='text-brightRed'>Us</span></h1>
